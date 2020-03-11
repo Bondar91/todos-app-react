e8be85dc48af67dc643514335b28d0b1dd29d28a
Krystian Bondaruk <krystian.bondaruk@gmail.com>
2020-03-11 19:19:45 +0100

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const Button = styled.button`
+  width: 100px;
+  height: 100px;
+  background-color: red;
+`;
 
 const App = () => (
   <div className="App">
@@ -9,6 +16,7 @@ const App = () => (
       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
+      <Button>My button</Button>
     </header>
   </div>
 );
